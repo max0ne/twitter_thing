588fd1118648d27668c4853cf56928410fd4079d
Mingfei Huang <himax1023@gmail.com>
2018-04-07 15:02:33 -0400

    split router from server

--- back/main.go
+++ back/main.go
@@ -13,6 +13,13 @@ import (
 	"github.com/gin-gonic/gin"
 )
 
+// Server - -
+type Server struct {
+	router *gin.Engine
+	store  *db.Store
+	tables globalTables
+}
+
 type globalTables struct {
 	userTable     *db.Table
 	tweetTable    *db.Table
@@ -52,7 +59,7 @@ func sendObj(c *gin.Context, key string, obj interface{}) {
 }
 
 // RESTFul Apis
-func signup(c *gin.Context) {
+func (s *Server) signup(c *gin.Context) {
 	username := c.PostForm("username")
 	password := c.PostForm("password")
 
@@ -62,7 +69,7 @@ func signup(c *gin.Context) {
 	}
 
 	// check whether the user already exists
-	oldUser, err := model.GetUser(username, tables.userTable)
+	oldUser, err := model.GetUser(username, s.tables.userTable)
 	if cerr(c, err) {
 		return
 	}
@@ -72,7 +79,7 @@ func signup(c *gin.Context) {
 		return
 	}
 
-	err = model.SaveUser(model.NewUser(username, password), tables.userTable)
+	err = model.SaveUser(model.NewUser(username, password), s.tables.userTable)
 	if cerr(c, err) {
 		return
 	}
@@ -89,11 +96,11 @@ func signup(c *gin.Context) {
 	})
 }
 
-func login(c *gin.Context) {
+func (s *Server) login(c *gin.Context) {
 	username := c.PostForm("username")
 	password := c.PostForm("password")
 
-	user, err := model.GetUserWithPassword(username, tables.userTable)
+	user, err := model.GetUserWithPassword(username, s.tables.userTable)
 	if cerr(c, err) {
 		return
 	}
@@ -120,23 +127,23 @@ func login(c *gin.Context) {
 	})
 }
 
-func unregister(c *gin.Context) {
+func (s *Server) unregister(c *gin.Context) {
 	user := middleware.GetUser(c)
 	if user == nil {
 		c.JSON(http.StatusUnauthorized, gin.H{"status": "user does not exist"})
 		return
 	}
 
-	model.DeleteUser(*user, tables.userTable)
+	model.DeleteUser(*user, s.tables.userTable)
 	c.JSON(200, gin.H{
 		"status":   "posted",
 		"username": user.Uname,
 	})
 }
 
-func getUser(c *gin.Context) {
+func (s *Server) getUser(c *gin.Context) {
 	username := c.Param("username")
-	user, err := model.GetUser(username, tables.userTable)
+	user, err := model.GetUser(username, s.tables.userTable)
 	if cerr(c, err) {
 		return
 	}
@@ -147,10 +154,10 @@ func getUser(c *gin.Context) {
 	sendObj(c, "user", *user)
 }
 
-func createNewTweet(c *gin.Context) {
+func (s *Server) createNewTweet(c *gin.Context) {
 	content := c.PostForm("content")
 	tweet := model.NewTweet(middleware.GetUser(c).Uname, content)
-	err := model.PublishNewTweet(tweet, tables.followerTable, tables.tweetTable, tables.bucketTable, tables.postedByTable)
+	err := model.PublishNewTweet(tweet, s.tables.followerTable, s.tables.tweetTable, s.tables.bucketTable, s.tables.postedByTable)
 	if cerr(c, err) {
 		return
 	}
@@ -161,10 +168,10 @@ func createNewTweet(c *gin.Context) {
 	})
 }
 
-func deleteTweet(c *gin.Context) {
+func (s *Server) deleteTweet(c *gin.Context) {
 	user := middleware.GetUser(c)
 	tid := c.Param("tid")
-	tweet, err := model.GetTweet(tid, tables.tweetTable)
+	tweet, err := model.GetTweet(tid, s.tables.tweetTable)
 	if cerr(c, err) {
 		return
 	}
@@ -174,63 +181,63 @@ func deleteTweet(c *gin.Context) {
 		return
 	}
 
-	model.DelTweet(tid, tables.tweetTable)
+	model.DelTweet(tid, s.tables.tweetTable)
 }
 
-func follow(c *gin.Context) {
+func (s *Server) follow(c *gin.Context) {
 	uname := c.PostForm("uname")
 	if uname == "" {
 		sendErr(c, http.StatusBadRequest, "uname required")
 		return
 	}
 
-	model.Follow(*middleware.GetUser(c), uname, tables.followingTable, tables.followerTable)
+	model.Follow(*middleware.GetUser(c), uname, s.tables.followingTable, s.tables.followerTable)
 }
 
-func unfollow(c *gin.Context) {
+func (s *Server) unfollow(c *gin.Context) {
 	uname := c.PostForm("uname")
 	if uname == "" {
 		sendErr(c, http.StatusBadRequest, "uname required")
 		return
 	}
 
-	if cerr(c, model.Unfollow(*middleware.GetUser(c), uname, tables.followingTable, tables.followerTable)) {
+	if cerr(c, model.Unfollow(*middleware.GetUser(c), uname, s.tables.followingTable, s.tables.followerTable)) {
 		return
 	}
 
-	if cerr(c, model.UnfollowUserTweet(*middleware.GetUser(c), uname, tables.bucketTable)) {
+	if cerr(c, model.UnfollowUserTweet(*middleware.GetUser(c), uname, s.tables.bucketTable)) {
 		return
 	}
 }
 
 // get users whom i am following
-func getFollowing(c *gin.Context) {
+func (s *Server) getFollowing(c *gin.Context) {
 	sendObj(c, "items",
 		model.GetUsers(
-			model.GetFollowing((*middleware.GetUser(c)).Uname, tables.followingTable), tables.userTable,
+			model.GetFollowing((*middleware.GetUser(c)).Uname, s.tables.followingTable), s.tables.userTable,
 		))
 }
 
 // get users whom i
-func getFollower(c *gin.Context) {
+func (s *Server) getFollower(c *gin.Context) {
 	sendObj(c, "items",
 		model.GetUsers(
-			model.GetFollowers((*middleware.GetUser(c)).Uname, tables.followerTable), tables.userTable,
+			model.GetFollowers((*middleware.GetUser(c)).Uname, s.tables.followerTable), s.tables.userTable,
 		))
 }
 
-func getUserTweets(c *gin.Context) {
+func (s *Server) getUserTweets(c *gin.Context) {
 	uname := c.Param("username")
 	if uname == "" {
 		sendErr(c, http.StatusBadRequest, "uname required")
 		return
 	}
 
-	sendObj(c, "items", model.GetUserTweets(uname, tables.tweetTable, tables.postedByTable))
+	sendObj(c, "items", model.GetUserTweets(uname, s.tables.tweetTable, s.tables.postedByTable))
 }
 
-func getFeed(c *gin.Context) {
-	tweets, err := model.GetUserFeed(middleware.GetUser(c).Uname, tables.tweetTable, tables.bucketTable)
+func (s *Server) getFeed(c *gin.Context) {
+	tweets, err := model.GetUserFeed(middleware.GetUser(c).Uname, s.tables.tweetTable, s.tables.bucketTable)
 	if cerr(c, err) {
 		return
 	}
@@ -238,8 +245,8 @@ func getFeed(c *gin.Context) {
 	sendObj(c, "items", tweets)
 }
 
-func main() {
-
+// NewServer - make a server
+func NewServer() Server {
 	store := db.NewStore()
 	tables = globalTables{
 		userTable:      store.NewTable("userTable"),
@@ -250,32 +257,48 @@ func main() {
 		followingTable: store.NewTable("followingTable"),
 	}
 
+	s := Server{
+		store:  store,
+		tables: tables,
+	}
+	s.router = s.NewRouter()
+	return s
+}
+
+// NewRouter make a router
+func (s *Server) NewRouter() *gin.Engine {
 	router := gin.Default()
 	router.Use(cors.Default())
 
 	cookieStore := sessions.NewCookieStore([]byte("suer_secret_session_secret"))
 	router.Use(sessions.Sessions("ts", cookieStore))
-	router.Use(middleware.InjectUser(tables.userTable))
+	router.Use(middleware.InjectUser(s.tables.userTable))
 
-	router.POST("/user/signup", signup)
-	router.POST("/user/login", login)
-	router.POST("/user/unregister", middleware.RequireLogin, unregister)
-	router.GET("/user/get/:username", getUser)
+	router.POST("/user/signup", s.signup)
+	router.POST("/user/login", s.login)
+	router.POST("/user/unregister", middleware.RequireLogin, s.unregister)
+	router.GET("/user/get/:username", s.getUser)
 
-	router.POST("/user/follow", middleware.RequireLogin, follow)
-	router.POST("/user/unfollow", middleware.RequireLogin, unfollow)
+	router.POST("/user/follow", middleware.RequireLogin, s.follow)
+	router.POST("/user/unfollow", middleware.RequireLogin, s.unfollow)
 
-	router.GET("/user/following", middleware.RequireLogin, getFollowing)
-	router.GET("/user/follower", getFollower)
+	router.GET("/user/following", middleware.RequireLogin, s.getFollowing)
+	router.GET("/user/follower", s.getFollower)
 
-	router.POST("/tweet/new", middleware.RequireLogin, createNewTweet)
-	router.POST("/tweet/del/:tid", middleware.RequireLogin, deleteTweet)
+	router.POST("/tweet/new", middleware.RequireLogin, s.createNewTweet)
+	router.POST("/tweet/del/:tid", middleware.RequireLogin, s.deleteTweet)
 
-	router.GET("/tweet/user/:username", getUserTweets)
-	router.GET("/tweet/feed", middleware.RequireLogin, getFeed)
-	router.GET("/db", func(c *gin.Context) {
-		sendObj(c, "", store.GetM())
-	})
+	router.GET("/tweet/user/:username", s.getUserTweets)
+	router.GET("/tweet/feed", middleware.RequireLogin, s.getFeed)
 
-	router.Run()
+	if gin.IsDebugging() {
+		router.GET("/db", func(c *gin.Context) {
+			sendObj(c, "", s.store.GetM())
+		})
+	}
+	return router
+}
+
+func main() {
+	NewServer().router.Run()
 }
